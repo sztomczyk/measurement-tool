3abb87b132cc0d61973b3809c8ccec2c5e9a331d
Szymon Tomczyk <szymon.tomczyk26@gmail.com>
2022-03-22 09:37:57 +0100

    init

---
+++ src/main/java/com/tomczyk/measurementstool/MeasurementsToolApplication.java
@@ -0,0 +1,13 @@
+package com.tomczyk.measurementstool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeasurementsToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeasurementsToolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tomczyk/measurementstool/MeasurementsToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tomczyk.measurementstool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeasurementsToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
